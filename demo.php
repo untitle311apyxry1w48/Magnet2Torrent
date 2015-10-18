b8f0f7b01dabfbece338fa1d7c30d98f569ebc6d
untitle311apyxry1w48 <untitle311apyxry1w48@gmail.com>
2015-10-18 19:11:08 +0900

    Create demo.php

---
+++ demo.php
@@ -0,0 +1,18 @@
+<?php
+//ver1.0
+	
+
+$url = 'http://torrent.whiteholic.net/FC8A15A2FAF2734DBB1DC5F7AFDC5C9BEAEB1F59';
+
+$ch = curl_init();
+curl_setopt ($ch, CURLOPT_URL, $url);
+curl_setopt ($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+
+print_r(json_decode($result));
+
+
+
+?>
